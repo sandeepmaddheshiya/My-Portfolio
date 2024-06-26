ecaa7c8d4cb0e8e7fa0e530170e54e9c65896f40
Sandeep Maddheshiya <113048079+sandeepmaddheshiya@users.noreply.github.com>
2024-06-26 12:40:50 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -3,22 +3,22 @@ export const experienceData = [
         id: 1,
         company: 'Mobiloitte',
         jobtitle: 'Software Developer Intern',
-        startYear: '19th Feb',
-        endYear: '28th March 2024'
+        startYear: 'Feb',
+        endYear: 'April 2024'
     },
     {
         id: 2,
         company: 'Topperworld',
         jobtitle: 'APP Development Intern',
-        startYear: ' 1st Aug',
-        endYear: '31st Jan 2024'
+        startYear: 'Aug',
+        endYear: 'Jan 2024'
     },
     {
         id: 3,
         company: 'TechnoHacks EduTech',
         jobtitle: 'Java Developer Intern',
-        startYear: '16th Aug',
-        endYear: '16th Sept 2023'
+        startYear: 'Aug',
+        endYear: 'Sept 2023'
     },
     
     
